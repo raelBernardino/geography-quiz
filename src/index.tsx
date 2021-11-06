4e7b737457021ffd7ee15d9d3773e106f21f61b5
Rael Bernardino <bernardino1207@gmail.com>
2021-11-06 02:03:34 -0700

    Initial theme creation

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    palette: {
+      primary: {
+        main: string,
+        light: string
+      }
+    }
+  }
+  interface PaletteOptions {
+    palette?: {
+      primary?: {
+        main?: string,
+        light?: string,
+      }
+    }
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFCD1B",
+      light: "#FAF1D3"
+    },
+    secondary: {
+      main: "#7C684B",
+      light: "#BFAD93"
+    }
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
